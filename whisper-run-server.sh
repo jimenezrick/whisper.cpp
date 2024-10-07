3fea07888cc49cad9861f057ba5bb382dac33016
Ricardo Catalinas Jiménez <r@untroubled.be>
2024-10-07 16:03:53 +0100

    Tweaks to rebase master

--- whisper-run-server.sh
+++ whisper-run-server.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
-# https://huggingface.co/ggerganov/whisper.cpp/resolve/main/ggml-large-v3-q5_0.bin?download=true
-MODEL=models/ggml-large-v3-q5_0.bin
-CUDAENV=/mnt/scratch-nvme/ricardo/CUDA12/cuda.env
+set -eu
+
+# See: https://github.com/ggerganov/whisper.cpp/tree/master/models
+# https://huggingface.co/ggerganov/whisper.cpp/resolve/main/ggml-large-v3-turbo-q5_0.bin?download=true
+MODEL=models/ggml-large-v3-turbo-q5_0.bin
+CUDAENV=~/bin/cuda.env
 
 SCRIPT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" &> /dev/null && pwd)
 
@@ -10,7 +13,7 @@ SCRIPT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" &> /dev/null && pwd)
 	cd $SCRIPT_DIR
 
 	source $CUDAENV
-	env WHISPER_CUDA=1 make -j server
+	env GGML_CUDA=1 make -j8 server
 
 	./server --model $MODEL
 )
