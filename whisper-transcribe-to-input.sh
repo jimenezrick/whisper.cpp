31b7bc6167a74a3f0ae3aaa9ce0b45bba404b7de
Ricardo Catalinas Jiménez <r@untroubled.be>
2024-10-18 16:30:31 +0100

    sox: Be less selective with the ALSA input device
    This still matches my microphone just fine, for some reason stop
    working before.

--- whisper-transcribe-to-input.sh
+++ whisper-transcribe-to-input.sh
@@ -6,7 +6,7 @@ set -e
 SOX_EFFECT='silence 1 0.2 2% 1 1.0 2%'
 
 # Can also use: ffmpeg -f alsa -i default -ar 16000 -y /tmp/whisper.wav
-uxterm -r -e bash -c "env AUDIODEV=hw:0 AUDIODRIVER=alsa rec --rate 16k --type wav /tmp/whisper.wav $SOX_EFFECT"
+uxterm -r -e bash -c "env AUDIODRIVER=alsa rec --rate 16k --type wav /tmp/whisper.wav $SOX_EFFECT"
 curl -s 127.0.0.1:8080/inference -H "Content-Type: multipart/form-data" -F file="@/tmp/whisper.wav" -F response_format="text" >/tmp/whisper.txt
 
 if [[ $1 = emacs ]]
