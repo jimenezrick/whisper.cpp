a4f4257bc2eee94624aad5f66b9f79727cdab4dc
Ricardo Catalinas Jiménez <r@untroubled.be>
2025-05-08 12:41:08 +0100

    Imprive client script to work with Emacs command voice-transcribe

--- whisper-transcribe-to-input.sh
+++ whisper-transcribe-to-input.sh
@@ -9,9 +9,13 @@ SOX_EFFECT='silence 1 0.2 2% 1 1.0 2%'
 uxterm -r -e bash -c "env AUDIODRIVER=alsa rec --rate 16k --type wav /tmp/whisper.wav $SOX_EFFECT"
 curl -s 127.0.0.1:8080/inference -H "Content-Type: multipart/form-data" -F file="@/tmp/whisper.wav" -F response_format="text" >/tmp/whisper.txt
 
-if [[ $1 = emacs ]]
+if [[ $1 = emacsclient ]]
 then
-	emacsclient  -e "(with-current-buffer \"${EMACS_BUFFER}\" (insert \"$(tr "\n" " " </tmp/whisper.txt)\"))"
+	emacsclient -e "(with-current-buffer \"${EMACS_BUFFER}\" (insert \"$(tr "\n" " " </tmp/whisper.txt | sed 's/^[ \t]*//;s/[ \t]*$//')\"))"
+elif [[ $1 = xdotool ]]
+then
+	xdotool type --delay=0 "$(tr "\n" " " </tmp/whisper.txt | sed 's/^[ \t]*//;s/[ \t]*$//')"
 else
-	xdotool type --delay=0 "$(tr "\n" " " </tmp/whisper.txt)"
+	cat /tmp/whisper.txt | tr "\n" " " | sed 's/^[ \t]*//;s/[ \t]*$//'
+	echo
 fi
